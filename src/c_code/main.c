bbf72580bce45576c0c4a33596434a69e7df67b4
Benjamin Herrera Navarro <caminlobo@gmail.com>
2021-06-18 07:21:54 -0500

    Test with hello world

--- src/c_code/main.c
+++ src/c_code/main.c
@@ -1,8 +1,18 @@
 
 
+void prints(char* string)
+{
+    char* c = string;
+    volatile char* d = (char*)0xffff;  
+    while(*c)
+    {
+        *d = *c;
+        c++;
+    }
+}
 
 
 int main()
 {
-    
+    prints("Hello QuantaRV!\n");
 }
