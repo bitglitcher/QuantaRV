c52ac1807cbce6dcdbbfb8873bb048ec50ee7845
Benjamin Herrera Navarro <caminlobo@gmail.com>
2021-06-18 06:22:52 -0500

    add support for more demultiplexer inputs for ALU and register file

--- src/global_pkg.sv
+++ src/global_pkg.sv
@@ -2,10 +2,11 @@
 package global_pkg;
 
 
-typedef enum logic[3:0] { REG_SRC, I_IMM_SRC } sr2_src_t;
+typedef enum logic[3:0] { REG_SRC, I_IMM_SRC, ALU_PC_SRC } sr2_src_t;
+typedef enum logic[3:0] { REG_SRC2, ALU_U_IMM_SRC, ALU_4, ALU_J_IMM, I_IMM_SRC1 } sr1_src_t;
 typedef enum logic[3:0] { ALU_INPUT, U_IMM_SRC, AUIPC_SRC, LOAD_SRC, PC_SRC, CSR_SRC } regfile_src_t;
 typedef enum logic[3:0] { J_IMM, B_IMM, I_IMM, EPC } jmp_target_src_t;
-typedef enum logic[3:0] { MEM_NONE, LOAD_DATA, STORE_DATA} memory_operation_t;
+typedef enum logic[3:0] { MEM_NONE, LOAD_DATA, STORE_DATA, FETCH_DATA} memory_operation_t;
 typedef enum logic[3:0] { CSR_SET, CSR_CLEAR, CSR_WRITE } write_mode_t;
 typedef enum logic[3:0] { VAL_IR, VAL_PC, VAL_ADDR } val_src_t;
 typedef enum logic [1:0] { BYTE, HALF_WORD, WORD } access_size_t;
