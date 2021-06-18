1b3fe73c823aa355c0929c3d4005a66c33a3c45f
Benjamin Herrera Navarro <caminlobo@gmail.com>
2021-06-18 06:27:33 -0500

    modify decoder to use outputs instead of interface.

--- src/decoder.sv
+++ src/decoder.sv
@@ -1,30 +1,40 @@
 module decoder
 (
     input logic [31:0] IR,
-    decode decode_bus
+    output logic [6:0] opcode,
+    output logic [4:0] rd,
+    output logic [2:0] funct3,
+    output logic [4:0] rs1,
+    output logic [4:0] rs2,
+    output logic [6:0] funct7,
+    output logic [11:0] i_imm,
+    output logic [11:0] s_imm,
+    output logic [12:0] b_imm,
+    output logic [31:0] u_imm,
+    output logic [20:0] j_imm
 );
 
 
 
-assign decode_bus.opcode = IR [6:0];
-assign decode_bus.rd = IR [11:7];
-assign decode_bus.funct3 = IR [14:12];
-assign decode_bus.rs1 = IR [19:15];
-assign decode_bus.rs2 = IR [24:20];
-assign decode_bus.funct7 = IR [31:25];
-assign decode_bus.i_imm = IR [31:20];
-assign decode_bus.s_imm = {IR [31:25], IR[11:7]};
-assign decode_bus.b_imm [0:0] = 1'b0;
-assign decode_bus.b_imm [11:11] = IR [7:7];
-assign decode_bus.b_imm [4:1] = IR [11:8];
-assign decode_bus.b_imm [10:5] = IR [30:25];
-assign decode_bus.b_imm [12:12] = IR [31:31];
-assign decode_bus.u_imm = {IR [31:12], 12'b000000000000};
-assign decode_bus.j_imm [0:0] = 1'b0;
-assign decode_bus.j_imm [19:12] = IR [19:12];
-assign decode_bus.j_imm [11:11] = IR [20:20];
-assign decode_bus.j_imm [10:1] = IR [31:21];
-assign decode_bus.j_imm [20:20] = IR [31:31];
+assign opcode = IR [6:0];
+assign rd = IR [11:7];
+assign funct3 = IR [14:12];
+assign rs1 = IR [19:15];
+assign rs2 = IR [24:20];
+assign funct7 = IR [31:25];
+assign i_imm = IR [31:20];
+assign s_imm = {IR [31:25], IR[11:7]};
+assign b_imm [0:0] = 1'b0;
+assign b_imm [11:11] = IR [7:7];
+assign b_imm [4:1] = IR [11:8];
+assign b_imm [10:5] = IR [30:25];
+assign b_imm [12:12] = IR [31:31];
+assign u_imm = {IR [31:12], 12'b000000000000};
+assign j_imm [0:0] = 1'b0;
+assign j_imm [19:12] = IR [19:12];
+assign j_imm [11:11] = IR [20:20];
+assign j_imm [10:1] = IR [31:21];
+assign j_imm [20:20] = IR [31:31];
 
 
 endmodule
