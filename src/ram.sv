9b88aaa0492fea85d2583356210fa75eaf7d0194
Benjamin Herrera Navarro <caminlobo@gmail.com>
2021-12-30 19:37:21 -0600

    fix wishbone bus timings

--- src/ram.sv
+++ src/ram.sv
@@ -21,30 +21,46 @@ reg [31:0] mem [SIZE-1:0];
 
 initial begin
     $readmemh("c_code/ROM.hex", mem);
+    DAT_I = 0;
 end
 
+typedef enum logic { IDDLE, CYCLE } state_t;
+state_t state = IDDLE;
 always@(posedge clk)
 begin
-    if(CYC & STB)
-    begin
-        ACK = 1'b1;   
-        RTY = 1'b0;   
-        ERR = 1'b0;   
-        if(WE)
+    case(state)
+        IDDLE:
         begin
-            mem[ADR[31:2]] = DAT_O;            
+            if(CYC & STB)
+            begin
+                ACK <= 1'b1;   
+                RTY <= 1'b0;   
+                ERR <= 1'b0;   
+                if(WE)
+                begin
+                    mem[ADR[31:2]] <= DAT_O;            
+                end
+                else
+                begin
+                    DAT_I <= mem[ADR[31:2]];             
+                end
+                state <= CYCLE;
+            end
+            else
+            begin                
+                ACK <= 1'b0;   
+                RTY <= 1'b0;   
+                ERR <= 1'b0;    
+            end
         end
-        else
+        CYCLE:
         begin
-            DAT_I = mem[ADR[31:2]];             
+            ACK <= 1'b0;   
+            RTY <= 1'b0;   
+            ERR <= 1'b0;    
+            state <= IDDLE;           
         end
-    end
-    else
-    begin        
-        ACK = 1'b0;   
-        RTY = 1'b0;   
-        ERR = 1'b0;   
-    end
+    endcase
 end
 
 
